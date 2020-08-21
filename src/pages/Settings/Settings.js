0cc6b8e1b8d51775f22d9bb2870faef1be1811e9
James Darby <james@Jamess-MacBook-Pro.local>
2020-08-21 10:00:58 +0100

    Laying settings foundation for changing theme

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, Row } from 'react-bootstrap';
 import './Settings.scss';
 
 export default function Settings() {
+    const [theme, setTheme] = useState('Forest Green');
+
+
     return(<Container id="settings-page">
         <Row style={{height: '2.5vh'}} noGutters/>
         <Row>
@@ -11,12 +14,27 @@ export default function Settings() {
             </div>
         </Row>
         <Row style={{height: '5vh'}}>
-           
         </Row>
         <Row style={{height: '65vh'}}>
-            <div className="main-container">
-                
-            </div>
+                <div className="main-container">
+                    <div>
+                        <h5>What task do you want to track?</h5>
+                        <input type="text" placeholder="Enter task name here"/>
+                    </div>
+                    <div>
+                        <h5>Choose your theme</h5>
+                        <h5>Currently Active: {theme}</h5>
+                        <select>
+                            <option value="">Select...</option>
+                            <option value="times">Times</option>
+                            <option value="minutes">Minutes</option>
+                            <option value="hours">Hours</option>
+                            <option value="km">KM</option>
+                            <option value="pages">Pages</option>
+                            <option value="custom">Other...</option>
+                        </select>
+                    </div>
+                </div>
         </Row>
     </Container>)
 }
